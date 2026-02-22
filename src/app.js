b0ee13b92b78fc7320d45f9afab7a1ad45f5e529
wiam <boukirwiam3@gmail.com>
2026-02-22 20:13:27 +0000

    Update welcome message on main branch

--- src/app.js
+++ src/app.js
@@ -2,25 +2,17 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-let tasks = [
-  { id: 1, title: "Initial task", completed: true },
-  { id: 2, title: "Install Git and Node.js", "completed": true },
-  { id: 2, title: "Learn DevOps basics", completed: false }
-  
-];
+
+const tasksRouter = require('./routes/tasks');
+
+
 
 app.get('/', (req, res) => {
-  res.json({ message: "DevOps Task Manager API is running(Lab1)" });
+  res.json({ message: "Welcome from MAIN branch" });
+
 });
 
-app.get('/tasks', (req, res) => {
-  res.json(tasks);
-});
 
-app.post('/tasks', (req, res) => {
-  const newTask = { id: tasks.length+1, title: req.body.title, completed: false };
-  tasks.push(newTask);
-  res.status(201).json(newTask);
-});
+app.use('/tasks',tasksRouter);
 
 app.listen(3000, ()=> console.log("API running on port 3000"));
