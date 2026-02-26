427d0fce3e72ab82afdff7c673abff147c14a71f
wiam <boukirwiam3@gmail.com>
2026-02-26 19:16:21 +0000

    Add Jest health-check test

--- src/app.js
+++ src/app.js
@@ -7,13 +7,16 @@ const tasksRouter = require('./routes/tasks');
 
 
 
-app.get('/', (req, res) => {
-  res.json({ message: "Welcome from FEATURE branch" }); 
-});
+pp.get('/', (req, res) => {
+ res.json({ message: "Task Manager API running (Lab2)" });
+}); 
+
 
 
 app.use('/tasks',tasksRouter);
 
-app.listen(3000, ()=> console.log("API running on port 3000"));
-// CI test change
-console.log("Testing CI with a Pull Request"); 
+if (require.main === module) {
+ app.listen(3000, () => console.log("API running on port 3000"));
+}
+module.exports = app; 
+
